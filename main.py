98b546698eaa78d64f672494547b653855155cae
I.P. de Jong <s3174034@fwn-bborg-edu-74-45.fmnsedu.rug.nl>
2018-02-26 13:54:50 +0100

    Found that the known learning does not fit the objective

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ from datetime import datetime
 import time
 
 date_format = "%d/%m/%Y"
-a = datetime.strptime('25/2/2018', date_format)
-b = datetime.strptime('26/2/2018', date_format)
-
 days = ['25/2/2018', '24/2/2018']
-dtDays = []
 diffDays = []
 for day in days:
-	dtDays = datetime.strptime(day, date_format)
-	print datetime.strptime(day, date_format) - datetime.strptime(time.strftime("%d/%m/%Y",date_format)
-#for day in dtDays:
-#	diffDays = day - datetime.date.today() 
-#print diffDays
+	
+	listDay = datetime.strptime(time.strftime("%d/%m/%Y"), date_format)
+	currDay = datetime.strptime(day, date_format)
+	#diffDays becomes a list of no. days since date in days (so changes every day)
+	diffDays.append((listDay-currDay).days)
+	
+meals = ['pizza', 'shoarma']
 
+meals.reshape(-1,1)
+diffDays.reshape(-1,1)
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(diffDays,meals)
+
+prediction = clf.predict(0)
+
+print prediction
