4a53017e6a7048dfca251cbb4a88982b3e82f12b
I.P. de Jong <s3174034@fwn-bborg-edu-74-45.fmnsedu.rug.nl>
2018-02-26 14:43:33 +0100

    Groundwork for expanding the dataset implemented and tested

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from datetime import datetime
 import time
 
 date_format = "%d/%m/%Y"
-days = ['25/2/2018', '24/2/2018']
+dayFile = open("days.txt", "r")
+days = []
+for line in dayFile:
+	days.append(line.replace('\n',''))
+dayFile.close()
+#print days
 diffDays = []
 for day in days:
 	
@@ -14,15 +19,47 @@ for day in days:
 	#diffDays becomes a list of no. days since date in days (so changes every day)
 	diffDays.append((listDay-currDay).days)
 	
-meals = ['pizza', 'shoarma']
+mealFile = open("meals.txt", "r")
+meals = []
+for line in mealFile:
+	meals.append(line.replace('\n',''))
+mealFile.close()
 
-meals.reshape(-1,1)
-diffDays.reshape(-1,1)
+'''
+predictionResults = [] #This should be made ofcourse
 
-clf = tree.DecisionTreeClassifier()
+print "Top predictions are"
+print "1.",predictionResults[0]
+print "2.",predictionResults[1]
+print "3.",predictionResults[2]
+'''
 
-clf = clf.fit(diffDays,meals)
+print "Top predictions are"
+print "1.",meals[0]
+print "2.",meals[1]
+#print "3.",meals[2]
+choice = input("Which of these are you eating tonight? If none press 0.\n")
+dayFile = open("days.txt", "a")
+mealFile = open("meals.txt", "a")
 
-prediction = clf.predict(0)
+if (choice <4 and choice >0):
+	#add the chosen meal to the meals files
+	#mealFile.write(predictionResults[choice-1])
+	mealFile.write(meals[choice-1])
+	mealFile.write("\n")
+	#add the current date to the dates files
+	today = time.strftime("%d/%m/%Y")
+	dayFile.write(today)
+	dayFile.write("\n")
 
-print prediction
+if (choice == 0):
+	newMeal = raw_input("What will you be eating tonight?\n")
+	newMeal = newMeal.lower()
+	mealFile.write(newMeal)
+	mealFile.write("\n")
+	today = time.strftime("%d/%m/%Y")
+	dayFile.write(today)
+	dayFile.write("\n")
+dayFile.close()
+mealFile.close()
+
