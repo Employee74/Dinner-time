b25507f86bc68253952afbf1c6b425fc85004074
Ivo de Jong <ivopascal@hotmail.com>
2018-02-27 19:18:37 +0100

    Worked on some thing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #Machine learning to predict what should be for dinner
 
-from sklearn import tree
+from sklearn.svm import SVR
 from datetime import datetime
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 date_format = "%d/%m/%Y"
 dayFile = open("days.txt", "r")
@@ -27,20 +28,35 @@ for line in mealFile:
 mealFile.close()
 
 def predict_meal(dates,meals,x):
-	diffDays = np.reshape(dates,len(dates), 1))
-	svr_len = SVR(kernel= 'linear', C=1e3)
-	svr_poly = SVR(kernel = 'ploy', C=1e3, degree = 2)
+	diffDays = np.reshape(dates,len(dates), 1)
+	svr_lin = SVR(kernel= 'linear', C=1e3)
+	svr_poly = SVR(kernel = 'poly', C=1e3, degree = 2)
 	svr_rbf = SVR(kernel = 'rbf', C=1e3, gamma =0.1)
 	svr_lin.fit(diffDays, meals)
 	svr_poly.fit(diffDays,meals)
 	svr_rbf.fit(diffDays,meals)
-	plt.scatter(
+	"""
+	plt.scatter(diffDays,meals, color = 'black', label = 'Data')
+	plt.plot(diffDays, svr_rbf.predict(days), color = 'red', label = 'RBF model')
+	plt.plot(diffDays, svr_lin.predict(days), color = 'green', label = 'Linear model')
+	plt.plot(diffDays, svr_poly.predict(days), color = 'blue', label = 'Polynomial model')
+	plt.xlabel("Days")
+	plt.ylabel("Meals")
+	plt.title("SVM meals")
+	plt.legend()
+	plt.show()
+	"""
+	return svr_rbf.predict(x)[0],svr_lin.predict(x)[0],svr_poly.predict(x)[0]
+	
+predicted_meal = predict_meal(diffDays,meals,29)
 
-print "Top predictions are"
-print "1.",meals[0]
-print "2.",meals[1]
-#print "3.",meals[2]
-choice = input("Which of these are you eating tonight? If none press 0.\n")
+print(predicted_meal)
+
+print ("Top predictions are")
+print ("1.",meals[0])
+print ("2.",meals[1])
+print ("3.",meals[2])
+choice = int( input("Which of these are you eating tonight? If none press 0.\n"))
 dayFile = open("days.txt", "a")
 mealFile = open("meals.txt", "a")
 
@@ -55,7 +71,7 @@ if (choice <4 and choice >0):
 	dayFile.write("\n")
 
 if (choice == 0):
-	newMeal = raw_input("What will you be eating tonight?\n")
+	newMeal = input("What will you be eating tonight?\n")
 	newMeal = newMeal.lower()
 	mealFile.write(newMeal)
 	mealFile.write("\n")
